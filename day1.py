50123fb1561863d53d85071952ff4d61c724c243
bhavsarvishwa <vishwa.bhavsar29@gmail.com>
2022-11-06 12:07:30 -0500

    Create day1.py

---
+++ day1.py
@@ -0,0 +1,8 @@
+with open("day1_input") as inp:
+    nums = inp.readlines()
+nums = [int(num.rstrip()) for num in nums]
+increased = 0
+for i, num in enumerate(nums[3:]):
+    if num > nums[i]:
+        increased +=1
+print(increased)
